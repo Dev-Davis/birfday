6b990151f567657f65adc42f7fefb217dfeabe19
Dev Davis <sean.dev.davis@gmail.com>
2019-05-20 20:51:48 -0500

    login/register

--- src/javascripts/components/auth.js
+++
@@ -1,8 +0,0 @@
-import util from '../helpers/util';
-
-const domStringBuilder = () => {
-  const domString = '<h1>Auth</h1>';
-  util.printToDom('auth', domString);
-};
-
-export default { domStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase/app';
 import '../styles/main.scss';
-import auth from './components/auth';
+import auth from './components/Auth/auth';
 
 import apiKeys from './helpers/apiKeys.json';
 
-import birf from './components/birfday';
+import birf from './components/Birf/birfday';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
