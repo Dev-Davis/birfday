5b3a76f8c5c3e5d32580a607c6ca75dc2394ebf6
Dev Davis <sean.dev.davis@gmail.com>
2019-05-20 21:26:44 -0500

    completed authentication

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,19 @@
 import firebase from 'firebase/app';
 import '../styles/main.scss';
+
 import auth from './components/Auth/auth';
+import birf from './components/Birf/birfday';
+import myNavbar from './components/myNavbar/myNavbar';
+import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
 
-import birf from './components/Birf/birfday';
-
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  myNavbar.navbarEvents();
   auth.domStringBuilder();
   birf.domStringBuilder();
+  authData.checkLoginStatus();
 };
 
 init();
